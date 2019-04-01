d1f8e8b4d6a958222b8467a71e4e801fb27d5ceb
Paul Smith <paul.home47@gmail.com>
2019-04-01 14:08:37 +0100

    test note

--- src/test/java/com/post/db/TestTmpStorage.java
+++ src/test/java/com/post/db/TestTmpStorage.java
@@ -2,16 +2,25 @@ package com.post.db;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import com.post.serv.MyService;
 
-
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class TestTmpStorage {
-
+	
+	@Autowired
+	protected TmpStorage storage;
+	
 	@Test
 	public void testCheckAutowiring() {
 		MyService myServ = new MyService();
-		//assertEquals("some text goes here", myServ.checkText());
-		assertTrue(true);
+		assertEquals("some text goes here", myServ.checkText());
+		assertTrue(storage != null);
 	}
 
 	
